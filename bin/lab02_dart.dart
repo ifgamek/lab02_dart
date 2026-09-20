898428ab45eb7d3af7491403a6d4dbd02760c41c
ifgamek <andkategik@gmail.com>
2026-09-20 17:24:11 +0300

    add package ansicolor

--- bin/lab02_dart.dart
+++ bin/lab02_dart.dart
@@ -1,80 +1,87 @@
 import 'dart:io';
+
 import 'package:lab02_dart/todo.dart';
+import 'package:ansicolor/ansicolor.dart';
 
-  void printMenu() {
-    print('');
-    print('ToDo список');
-    print('add  - добавить задачу');
-    print('list   - показать все задачи');
-    print('done   - отметить выполненной ');
-    print('delete   - удалить задачу');
-    print('exit   - выйти');
+final AnsiPen greenPen = AnsiPen()..green();
+final AnsiPen redPen = AnsiPen()..red();
+final AnsiPen bluePen = AnsiPen()..blue();
+final AnsiPen yellowPen = AnsiPen()..yellow();
+void printMenu() {
+  print('');
+  print(yellowPen('ToDo список'));
+  print('add  - добавить задачу');
+  print('list   - показать все задачи');
+  print('done   - отметить выполненной ');
+  print('delete   - удалить задачу');
+  print('exit   - выйти');
+}
+
+void addTodo(List<Todo> todos) {
+  stdout.write('Название задачи: ');
+  String? input = stdin.readLineSync();
+
+  if (input == null || input.trim().isEmpty) {
+    print(redPen('ошибка : название не может быть пустым'));
+    return;
   }
+  int newId = todos.isEmpty ? 1 : todos.last.id + 1;
+  todos.add(Todo(id: newId, title: input.trim()));
+  print('задача добавлена');
+}
 
-  void addTodo(List<Todo> todos) {
-    stdout.write('Название задачи: ');
-    String? input = stdin.readLineSync();
+void listTodos(List<Todo> todos) {
+  if (todos.isEmpty) {
+    print('список задач пуст');
+    return;
+  }
+  print('');
+  for (var todo in todos) {
+    print(todo);
+  }
+}
 
-    if (input == null || input.trim().isEmpty) {
-      print('ошибка : название не может быть пустым');
+void completeTodo(List<Todo> todos) {
+  stdout.write('id задачи:');
+  String? input = stdin.readLineSync();
+  if (input == null) return;
+
+  int? id = int.tryParse(input.trim());
+  if (id == null) {
+    print('ошибка :введите число');
+    return;
+  }
+  for (var todo in todos) {
+    if (todo.id == id) {
+      todo.complete();
+      print('задача отмечена выполненной!');
       return;
     }
-    int newId = todos.isEmpty ? 1 : todos.last.id + 1;
-    todos.add(Todo(id: newId, title: input.trim()));
-    print('задача добавлена');
   }
+  print('задача с id $id не найдена');
+}
 
-  void listTodos(List<Todo> todos) {
-    if (todos.isEmpty) {
-      print('список задач пуст');
+void deleteTodo(List<Todo> todos) {
+  stdout.write('id задачи:');
+  String? input = stdin.readLineSync();
+  if (input == null) return;
+  int? id = int.tryParse(input.trim());
+
+  if (id == null) {
+    print('ошибка: введите число');
+    return;
+  }
+  for (int i = 0; i < todos.length; i++) {
+    if (todos[i].id == id) {
+      todos.removeAt(i);
+      print('задача удалена!');
       return;
     }
-    print('');
-    for (var todo in todos) {
-      print(todo);
-    }
   }
+  print('задача с ID $id не найдена');
+}
 
-  void completeTodo(List<Todo> todos) {
-    stdout.write('id задачи:');
-    String? input = stdin.readLineSync();
-    if (input == null) return;
-
-    int? id = int.tryParse(input.trim());
-    if (id == null) {
-      print('ошибка :введите число');
-      return;
-    }
-    for (var todo in todos) {
-      if (todo.id == id) {
-        todo.complete();
-        print('задача отмечена выполненной!');
-        return;
-      }
-    }
-    print('задача с id $id не найдена');
-  }
-
-  void deleteTodo(List<Todo> todos) {
-    stdout.write('id задачи:');
-    String? input = stdin.readLineSync();
-    if (input == null) return;
-    int? id = int.tryParse(input.trim());
-    
-    if (id == null) {
-      print('ошибка: введите число');
-      return;
-    }
-    for (int i = 0; i < todos.length; i++) {
-      if (todos[i].id == id) {
-        todos.removeAt(i);
-        print('задача удалена!');
-        return;
-      }
-    }
-    print('задача с ID $id не найдена');
-  }
-void main(){
+void main() {
   List<Todo> todos = [];
   printMenu();
   while (true) {
